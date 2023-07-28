360171b3a6a86157d86214dedb910727db402e6c
astasiempre <ero1494651@gmail.com>
2023-07-28 17:39:46 +0300

    add  lodash.throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = new Player('vimeo-player');
+const localStorageKey = 'videoplayer-current-time';
+
+
+const saveCurrentTime = (time) => {
+  localStorage.setItem(localStorageKey, time);
+};
+
+
+const loadAndSetCurrentTime = () => {
+  const currentTime = localStorage.getItem(localStorageKey);
+  if (currentTime) {
+    vimeoPlayer.setCurrentTime(parseFloat(currentTime));
+  }
+};
+
+
+const onTimeUpdate = throttle((event) => {
+  const currentTime = event.seconds;
+  saveCurrentTime(currentTime);
+}, 1000);
+
+
+vimeoPlayer.ready().then(() => {
+  loadAndSetCurrentTime();
+  vimeoPlayer.on('timeupdate', onTimeUpdate);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+const localStorageKey = 'feedback-form-state';
+
+// Function to save form data to local storage
+const saveFormData = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}, 500);
+
+// Function to load and set form data from local storage
+const loadAndSetFormData = () => {
+  const savedData = localStorage.getItem(localStorageKey);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email;
+    messageTextarea.value = formData.message;
+  }
+};
+
+// Event listener to track input changes and save form data
+form.addEventListener('input', () => {
+  saveFormData();
+});
+
+// Event listener to clear form and local storage on submit
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(formData);
+  localStorage.removeItem(localStorageKey);
+  emailInput.value = '';
+  messageTextarea.value = '';
+});
+
+// Load and set form data on page load
+loadAndSetFormData();
